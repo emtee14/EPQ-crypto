d6b0b5b68c4888b374fa7deff43319ef254673e0
Morgan <48140121+emtee14@users.noreply.github.com>
2021-10-17 21:25:54 +0100

    Block mining and verifying

--- crypto/blockchain.py
+++ crypto/blockchain.py
@@ -8,7 +8,7 @@ from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 
 """
 DB schema
-Block db - all blocks along with their data except transactions (block_number, hash, nonce, prev_block, timestamp)
+Block db - all blocks along with their data except transactions (block_number, hash, nonce, prev_block_hash, timestamp)
 Transation DB - contains all transactions along with the block they are asociated with (block_hash, sender, reciever, amount, signature, timestamp)
 """
 
@@ -17,6 +17,11 @@ class Transaction():
         self._tran_dict = transcation_dict
         self.signed = signed
         self.import_tran()
+        self.valid = False
+        if signed:
+            self.signature = transcation_dict["signature"]
+            self._tran_dict.pop("signature")
+            self.verify_signature()
 
     def sign(self, priv_key: str) -> None:
         """Takes in private key and signs the transaction to prove it was sent by the owner
@@ -49,7 +54,6 @@ class Transaction():
             self.valid = True
         except ValueError:
             self.valid = False
-        return self.valid
 
     @property
     def tran_dict(self) -> Dict:
@@ -72,3 +76,56 @@ class Transaction():
         self.timestamp = self._tran_dict["timestamp"]
         if self.signed:
             self.signature = self._tran_dict["signature"]
+
+
+class Block():
+    def __init__(self, block_dict, mined: bool = True) -> None:
+        self.henry = "Smells"
+        self.block_dict = block_dict
+        self.mined = mined
+        self.valid = False
+        self.import_block()
+
+    def import_block(self):
+        self.number = self.block_dict["block_number"]
+        if self.mined:
+            self.hash = self.block_dict["hash"]
+            self.nonce = self.block_dict["nonce"]
+        self.prev_block = self.block_dict["prev_block_hash"]
+        self.timestamp = self.block_dict["timestamp"]
+        self.transactions = []
+        for transaction in self.block_dict["transactions"]:
+            tran_obj = Transaction(transaction, signed=True)
+            tran_obj.verify_signature()
+            if tran_obj.valid:
+                self.transactions.append(tran_obj)
+            else:
+                print("Fatal error somewhere invalid transaction saved to blockchain")
+
+    def mine_block(self):
+        nonce = 0
+        while self.mined == False:
+            temp_block = self.block_dict.copy()
+            temp_block["nonce"] = nonce
+            block_string = json.dumps(temp_block)
+            block_hash = SHA256.new(block_string.encode("UTF-8")).hexdigest()
+            if block_hash[:5] == "14605":
+                self.mined = True
+                self.hash = block_hash
+                self.nonce = nonce
+                self.block_dict["hash"] = block_hash
+            nonce +=1
+            
+
+    def verify_block(self):
+        block_no_hash = self.block_dict.copy()
+        block_no_hash.pop("hash")
+        block_no_hash["nonce"] = self.nonce
+        block_string = json.dumps(block_no_hash)
+        block_hash = SHA256.new(block_string.encode("UTF-8")).hexdigest()
+        if block_hash == self.hash:
+            self.valid = True
+        else:
+            self.valid = False
+
+
