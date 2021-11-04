e59a8882da7416bf2bedc54af1cd0cee9ebcb36c
Morgan <48140121+emtee14@users.noreply.github.com>
2021-11-04 16:32:38 +0000

    Transaction refractoring

---
+++ blockchain/transaction.py
@@ -0,0 +1,81 @@
+import json
+from typing import Any, Dict
+
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+
+
+class Transaction():
+    def __init__(self, sender: str, receiver: str, value: int, data: str,
+                 fee: int, signature: str = False, nonce: int = False) -> None:
+        self.sender = sender
+        self.receiver = receiver
+        self.value = value
+        self.data = data
+        self.fee = fee
+        self.signed = True if signature and nonce is not None else False
+        self.signature = signature if self.signed else None
+        self.nonce = nonce if self.signed else None
+
+    @property
+    def tran_dict(self, signed: bool = False) -> Dict:
+        tran_props = ["sender", "receiver", "value", "data", "fee"]
+        if signed:
+            tran_props += ["signature", "nonce"]
+        tran_dict = {}
+        for prop in tran_props:
+            tran_dict[prop] = getattr(self, prop)
+        return tran_dict
+
+    def sign(self, priv_string: str, nonce: int) -> None:
+        """Takes in private key and signs the transaction to prove it was
+           sent by the owner of the account
+
+        :param priv__string: Private key in PEM format
+        :type priv__string: str
+        :param nonce: Number used once to make transaction unique
+        :type nonce: int
+        """
+        if not self.signed:
+            tran_data = self.tran_dict.copy()
+            tran_data["nonce"] = nonce
+            transaction_string = json.dumps(tran_data)
+            priv_key = RSA.import_key(priv_string)
+            signer = PKCS115_SigScheme(priv_key)
+            tran_hash = SHA256.new(transaction_string.encode("UTF-8"))
+            signature = signer.sign(tran_hash)
+            self.signature = signature.hex()
+            self.nonce = nonce
+            self.signed = True
+
+    def verify_signature(self) -> bool:
+        """Checks if the signature of the transaction is valid
+
+        :return: Returns True if it is a valid signature and False if not
+        :rtype: bool
+        """
+        tran_dict = self.tran_dict.copy()
+        tran_dict["nonce"] = self.nonce
+        transaction_string = json.dumps(tran_dict)
+        tran_hash = SHA256.new(transaction_string.encode("UTF-8"))
+        pub_key = RSA.import_key(self.sender)
+        verifier = PKCS115_SigScheme(pub_key)
+        try:
+            verifier.verify(tran_hash, bytes.fromhex(self.signature))
+            self.valid = True
+        except ValueError:
+            self.valid = False
+            raise ValueError("Invalid transaction signature")
+
+    def __iter__(self) -> Any:
+        """Returns transaction in iter format for inserting into DB
+
+        :yield: Returns value for attribute
+        :rtype: Any
+        """
+        default_attr = ["sender", "receiver", "value", "data", "fee"]
+        if self.signed:
+            default_attr += ["signature", "nonce"]
+        for i in default_attr:
+            yield getattr(self, i)
