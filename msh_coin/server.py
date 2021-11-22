b0dfcad686c17bb87934e24f14a16d397b7cd05d
Morgan Thomas <morganrhysthoams14@gmail.com>
2021-11-22 14:12:40 +0000

    Clean Up

--- msh_coin/server.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-
-class Server():
-    def __init__(self) -> None:
-        self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket_server.bind(("0.0.0.0", 14605))
--- testing_test.py
+++
@@ -1,6 +0,0 @@
-from testing import block_test, tran_test
-
-if __name__ == "__main__":
-    print("Starting tests...")
-    tran_test.test()
-    block_test.test()
