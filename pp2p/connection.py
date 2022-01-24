c1d81986af0a401b6479d2f1a0b85b605b11c791
Morgan Thomas <morganrhysthoams14@gmail.com>
2022-01-24 18:56:54 +0000

    Working P2P Network

--- pp2p/connection.py
+++ pp2p/connection.py
@@ -5,18 +5,21 @@ import time
 
 
 class Connection(threading.Thread):
-    def __init__(self, parent_proc, sock_obj, uid, host, port) -> None:
+    def __init__(self, parent_proc, sock_obj, host, port, client) -> None:
         super(Connection, self).__init__()
         self.parent_proc = parent_proc
         self.stop = threading.Event()
+        self.client = client  # True if connection is client and connecting to another server
         self.sock = sock_obj
-        self.uid = uid
+        self.uid = None
         self.host = host
         self.port = port
         self.EOT_CHAR = 0x14.to_bytes(1, 'big')
         self.sock.settimeout(15)
+        self.ping_pong()
 
     def send(self, data: dict):
+        print(data)
         try:
             msg_str = json.dumps(data, sort_keys=True)
         except Exception:
@@ -25,7 +28,8 @@ class Connection(threading.Thread):
         self.sock.sendall(msg_bytes)
 
     def parse_message(self, msg: bytes) -> dict:
-        json_str = msg.decode("UTF-8")[:-1]
+        print(msg)
+        json_str = msg.decode("UTF-8")
         try:
             msg_dict = json.loads(json_str)
         except Exception:
@@ -42,20 +46,52 @@ class Connection(threading.Thread):
                 pass # error loggign
             if transmission != b"":
                 buffer += transmission
-                eot_position = buffer.find(self.EOT_CHAR)
-                while eot_position > 0:
-                    msg_bytes = buffer[:eot_position]
-                    buffer = buffer[eot_position+1:]
+                eot_pos = buffer.find(self.EOT_CHAR)
+                while eot_pos > 0:
+                    msg_bytes = buffer[:eot_pos]
+                    buffer = buffer[eot_pos+1:]
                     try:
                         msg_dict = self.parse_message(msg_bytes)
-                        self.parent_proc.message(msg_dict)
+                        self.parent_proc.callback(msg_dict)
                     except ValueError:
                         pass # logging thingy
-                    eot_position = buffer.find(self.EOT_CHAR)
+                    eot_pos = buffer.find(self.EOT_CHAR)
             time.sleep(0.05)
         self.sock.settimeout(None)
         self.sock.close()
         self.parent_proc.disconnected_node(self)
         # loggin stuff
+
+    def ping_pong(self):
+        if self.client:
+            self.send({
+                "type": "init_ping",
+                "data": {
+                    "id": self.parent_proc.id,
+                    "clients": self.parent_proc.client_tuples
+                    }
+                })
+        full_msg = False
+        buffer = b""
+        while not full_msg:
+            data = b""
+            data = self.sock.recv(4096)
+            if data != b"":
+                buffer += data
+                eot_pos = buffer.find(self.EOT_CHAR)
+                if eot_pos != -1:
+                    msg_bytes = buffer[:eot_pos]
+                    msg = self.parse_message(msg_bytes)
+                    break
+        self.parent_proc.connect_list += msg["data"]["clients"]
+        self.uid = msg["data"]["id"]
+        if msg["type"] == "init_ping":
+            self.send({
+                "type": "resp_pong",
+                "data": {
+                    "id": self.parent_proc.id,
+                    "clients": self.parent_proc.client_tuples
+                    }
+                })
 
 # https://github.com/macsnoeren/python-p2p-network/blob/development/p2pnetwork/nodeconnection.py
---
+++ pp2p/node.py
@@ -0,0 +1,128 @@
+import hashlib
+import socket
+import threading
+import time
+from typing import Dict, List
+
+from connection import Connection
+
+
+class Node(threading.Thread):
+    def __init__(self, host, port, callback, bootstrap,
+                 max_connections=0) -> None:
+        super(Node, self).__init__()
+
+        self.terminate_flag = threading.Event()
+        self.host = host
+        self.port = port
+        self.callback = callback
+        self.max_connections = max_connections
+
+        self.inbound = []
+        self.outbound = []
+        self.connect_list = bootstrap
+
+        self.id = hashlib.sha256((str(host)+str(port)+str(time.time())).encode("UTF-8")).hexdigest()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.message_recv = 0
+        self.message_send = 0
+
+    @property
+    def total_nodes(self):
+        return self.inbound + self.outbound
+
+    @property
+    def client_tuples(self):
+        temp = []
+        for i in self.outbound:
+            temp.append((i.host, i.port))
+        return temp
+
+    def init_sock(self):
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        self.sock.settimeout(5.0)
+        self.sock.listen(5)
+
+    def send_all(self, msg: Dict, exclude: List = []):
+        connections = self.total_nodes
+        for conn in connections:
+            if conn.uid not in exclude:
+                conn.send(msg)
+
+    def send(self, conn_id, msg):
+        try:
+            conn = self.total_nodes.index(conn_id)
+        except ValueError:
+            raise ValueError("Invalid node id or node disconnected")
+        conn.send(msg)
+
+    def create_conn(self, sock, host, port, client):
+        return Connection(self, sock, host, port, client)
+
+    def connect_node(self, host, port):
+        if host == self.host and port == self.port:
+            print("Cannot connect to self")
+            return False
+
+        for node in self.total_nodes:
+            if host == node.host and port == node.port:
+                print("Already connected to node")
+                return False
+        try:
+            if self.max_connections == 0 or len(self.total_nodes) < self.max_connections:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((host, port))
+
+                client_thread = self.create_conn(sock, host, port, True)
+                client_thread.start()
+                self.outbound.append(client_thread)
+            else:
+                print("Too many clients")
+        except Exception as e:
+            print("Unable to connect", e)
+            print()
+            return False
+
+    def reconnect_nodes(self):
+        for node in self.connect_list:
+            connected = False
+            for client in self.outbound:
+                if client.port == node[1] and client.host == node[0]:
+                    connected = True
+                    break
+            if not connected:
+                self.connect_node(node[0], node[1])
+                self.connect_list.remove(node)
+
+    def stop(self):
+        self.terminate_flag.set()
+
+    def run(self):
+        self.init_sock()
+        while not self.terminate_flag.is_set():
+            try:
+                conn, addr = self.sock.accept()
+                if self.max_connections == 0 or len(self.total_nodes) < self.max_connections:
+                    conn_thread = self.create_conn(conn, addr[0], addr[1], False)
+                    conn_thread.start()
+                    self.inbound.append(conn_thread)
+                else:
+                    conn.close()
+            except socket.timeout:
+                pass
+            self.reconnect_nodes()
+
+        print("Stopping")
+        for t in self.inbound:
+            t.stop()
+        for t in self.outbound:
+            t.stop()
+
+        time.sleep(1)
+
+        for t in self.inbound:
+            t.join()
+        for t in self.outbound:
+            t.join()
